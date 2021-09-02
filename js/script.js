50680f457786c1798bca4d4ad6a36aa7c582612a
Forklein <giuseppepisani2@gmail.com>
2021-09-02 15:59:59 +0200

    Set function for print option in select based on genre

--- js/script.js
+++ js/script.js
@@ -6,22 +6,41 @@ const app = new Vue({
     data: {
         test: 'test',
         data: '',
-    },
-    mounted: {
-
+        filterValue: '',
+        genre: []
     },
     methods: {
+        getData() {
+            axios
+                .get('https://flynn.boolean.careers/exercises/api/array/music')
+                .then((res) => {
+                    const obj = res.data.response;
+                    this.data = obj;
+                })
+                .catch((err) => {
+                    console.log(err)
+                })
+        },
+        getGenre() {
+            axios
+                .get('https://flynn.boolean.careers/exercises/api/array/music')
+                .then((res) => {
+                    const obj = res.data.response;
+                    const tempArr = [];
+                    obj.forEach((element, index) => {
+                        if (!tempArr.includes(element.genre)) {
+                            tempArr.push(element.genre)
+                        }
+                    })
+                    this.genre = tempArr;
+                })
+                .catch((err) => {
+                    console.log(err)
+                })
+        }
     },
     created() {
-        axios
-            .get('https://flynn.boolean.careers/exercises/api/array/music')
-            .then((res) => {
-                const obj = res.data.response;
-                console.log(obj);
-                this.data = obj;
-            })
-            .catch((err) => {
-                console.log(err)
-            })
+        this.getData();
+        this.getGenre();
     }
 })
