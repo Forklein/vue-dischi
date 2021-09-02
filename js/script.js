aa340091bb212124de43b9cd8df0e397c63dca9f
Forklein <giuseppepisani2@gmail.com>
2021-09-02 15:17:52 +0200

    Set v-for for axios request data

--- js/script.js
+++ js/script.js
@@ -5,8 +5,17 @@ const app = new Vue({
     el: '#app',
     data: {
         test: 'test',
+        data: '',
     },
     methods: {
-
+    },
+    created() {
+        axios
+            .get('https://flynn.boolean.careers/exercises/api/array/music')
+            .then((res) => {
+                const obj = res.data.response;
+                console.log(obj);
+                this.data = obj;
+            })
     }
 })
